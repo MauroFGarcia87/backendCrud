beb12029c2224798256fa2ef8e53f79787148be0
MauroFGarcia87 <maurofgarcia87@gmail.com>
2023-09-29 08:43:50 -0300

    Agregamos el index html y quedo lista la estructura para el backend

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
 import express from 'express';
+import res from 'express/lib/response';
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path';
+
 console.log('Hola BackEnd');
 
 // Crear un puerto para el servidor
 // Creo un instancia de express
 const app = express();
+
+//Los midlewares o configuraciones extra
+app.use(morgan('dev')); // Da informacion extra a la terminal
+app.use(cors()); //Permoite recibir peticiones  remotas
+
+//Permite reibir objetos en formato json
+app.use(express.json())
+app.use(express.urlencoded({extended:true}));
+
+//Mostrar una pagina por defecto
+console.log(path.join(__dirname, '../public'));
+app.use(express.static(path.join(__dirname, '../public')))
 
 // Crear un puerto con express
 app.set('port', process.env.PORT || 4000)
@@ -13,3 +30,11 @@ app.listen(app.get('port'), ()=>{
     console.log('Estoy en el puerto '+ app.get('port'));
 })
 
+// Crear Rutas
+app.get('/', (req, res) =>{
+    res.send('Hola Mundo desde Backend')
+})
+
+app.delete('/borrardatos', (req, res) =>{
+    res.send('borramos los datos')
+})
