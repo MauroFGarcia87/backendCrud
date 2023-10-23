13b09dc6ecfa510681e655e6a075a3eebb1c2fe0
MauroFGarcia87 <maurofgarcia87@gmail.com>
2023-10-23 10:09:44 -0300

    agragamo la logica para enviar el listado de la base de datos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -4,8 +4,16 @@ import Producto from "../models/producto";
 const productoCtrl = {};
 
 //Logica para obtener la lista de producto
-productoCtrl.listarProductos = (req, res) =>{
-    res.send('Listar Productos')
+productoCtrl.listarProductos = async(req, res) =>{
+
+    try {
+       // Crear un arreglo con todos los productos
+       const listaProductos = await Producto.find();
+       res.status(200).json(listaProductos);
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({mensaje: 'No se pudo obtener el listado de productos'})
+    }
 }
 
 productoCtrl.borrarProducto = (req, res) =>{
