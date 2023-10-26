d04d307358365b9480db7cbb76f1c7a060d4f427
MauroFGarcia87 <maurofgarcia87@gmail.com>
2023-10-26 19:28:46 -0300

    Cambiamos la ruta de la base de datos a mongo atlas

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 //Cadena de conexion a la base de datos
-const url = 'mongodb://127.0.0.1:27017/crudcafe';
+//const url = 'mongodb://127.0.0.1:27017/crudcafe';
+const url = 'mongodb+srv://maurofgarcia87:MauLet32@cluster0.nm6qcyv.mongodb.net/crudcafe';
 
 // Conectarce a la base de datos
 mongoose.connect(url);
